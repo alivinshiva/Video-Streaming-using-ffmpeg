bfa4293357b7fbbb661b9155617e8362aa6d5a75
Alivin Shiva <alivin612003@gmail.com>
2024-05-04 13:12:25 +0530

    project completed

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import VideoPlayer from "./VideoPlayer";
+import { useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const playerRef = useRef(null);
+  const videoLink =
+    "http://localhost:8000/uploads/test/7cff5c50-66f5-4d04-a098-6495f147c8a8/index.m3u8";
+
+  const videoPlayerOptions = {
+    controls: true,
+    reponsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // handling player
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Video player</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <VideoPlayer options={videoPlayerOptions} onReady={handlePlayerReady} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
